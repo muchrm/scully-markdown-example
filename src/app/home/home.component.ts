981c3804a5ad1ff0e9f394a00b28c9a0615412a0
Pongpanot Chuaysakun <muchrm.me@gmail.com>
2020-11-04 23:48:27 +0700

    add serve scully

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  links$: Observable<ScullyRoute[]> = this.scully.available$;
 
-  constructor() { }
+  constructor(private scully: ScullyRoutesService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // debug current pages
+    this.links$.subscribe((links) => {
+      console.log(links);
+    });
   }
-
 }
